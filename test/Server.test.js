9f8c1cd7f9b2564fb01d239631aeab89c423ec36
imoses <imoses.g@gmail.com>
2016-11-26 23:28:44 +0200

    adjusted test to verify multi-compiler mode is functioning correctly when more than one outputPath is present

--- test/Server.test.js
+++ test/Server.test.js
@@ -153,7 +153,7 @@ describe("Server", function() {
 			var instance = middleware(compiler, {
 				stats: "errors-only",
 				quiet: true,
-				publicPath: "/",
+				publicPath: "/"
 			});
 			app.use(instance);
 			listen = listenShorthand(done);
@@ -161,9 +161,9 @@ describe("Server", function() {
 		after(close);
 
 		it("request to both bundle files", function(done) {
-			request(app).get("/foo.js")
+			request(app).get("/js1/foo.js")
 			.expect(200, function() {
-				request(app).get("/bar.js")
+				request(app).get("/js2/bar.js")
 				.expect(200, done);
 			});
 		});
