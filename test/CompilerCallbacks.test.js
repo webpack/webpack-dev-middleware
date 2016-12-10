ba1c673a07e255d7bdeadffd199bf7ca9fbba72e
Kees Kluskens <kees@webduck.nl>
2016-12-10 23:14:57 +0100

    Fix error handling in compiler `watch()` and `run()`
    This changes it to be more like how webpack's watch mode handles it;
    if the compiler throws an error in `watch()` or `run()`, the middleware
    should continu to run instead of quitting.

---
+++ test/CompilerCallbacks.test.js
@@ -0,0 +1,40 @@
+var should = require("should");
+var middleware = require("../middleware");
+require("mocha-sinon");
+
+describe("CompilerCallbacks", function() {
+	var plugins = {};
+	var compiler = {
+		watch: function() {},
+		plugin: function(name, callback) {
+			plugins[name] = callback;
+		}
+	};
+	beforeEach(function() {
+		plugins = {};
+	});
+
+	it("watch error should be reported to console", function(done) {
+		var error = new Error("Oh noes!");
+		this.sinon.stub(compiler, "watch", function(opts, callback) {
+			callback(error);
+		});
+		this.sinon.stub(console, "error");
+		middleware(compiler);
+		should.strictEqual(console.error.callCount, 1);
+		should.strictEqual(console.error.calledWith(error.stack), true);
+		done();
+	});
+
+	it("options.error should be used on watch error", function(done) {
+		this.sinon.stub(compiler, "watch", function(opts, callback) {
+			callback(new Error("Oh noes!"));
+		});
+		middleware(compiler, {
+			error: function(err) {
+				err.should.startWith("Error: Oh noes!");
+				done();
+			}
+		});
+	});
+});
