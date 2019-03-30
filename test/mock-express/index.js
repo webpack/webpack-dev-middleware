517c5e0247717d0ff5e9b6a7f446d2c83bb4e29c
ULIVZ <472590061@qq.com>
2019-03-31 01:15:09 +0800

    chore: tweaks

--- test/mock-express/index.js
+++ test/mock-express/index.js
@@ -13,25 +13,27 @@ const mockRequest = (options = {}) =>
   );
 
 const mockResponse = (options = {}) => {
-  const res = {
-    cookie: jest.fn(),
-    clearCookie: jest.fn(),
-    download: jest.fn(),
-    format: jest.fn(),
-    json: jest.fn(),
-    jsonp: jest.fn(),
-    send: jest.fn(),
-    sendFile: jest.fn(),
-    sendStatus: jest.fn(),
-    setHeader: jest.fn(),
-    redirect: jest.fn(),
-    render: jest.fn(),
-    end: jest.fn(),
-    set: jest.fn(),
-    type: jest.fn(),
-    get: jest.fn(),
-    ...options,
-  };
+  const res = Object.assign(
+    {
+      cookie: jest.fn(),
+      clearCookie: jest.fn(),
+      download: jest.fn(),
+      format: jest.fn(),
+      json: jest.fn(),
+      jsonp: jest.fn(),
+      send: jest.fn(),
+      sendFile: jest.fn(),
+      sendStatus: jest.fn(),
+      setHeader: jest.fn(),
+      redirect: jest.fn(),
+      render: jest.fn(),
+      end: jest.fn(),
+      set: jest.fn(),
+      type: jest.fn(),
+      get: jest.fn(),
+    },
+    options
+  );
   res.status = jest.fn(() => res);
   res.vary = jest.fn(() => res);
   return res;
