8ec0b3f4ccfc749cd134123a63defa7170dbc6fa
Yuta Hiroto <git@about-hiroppy.com>
2019-07-22 11:16:48 +0100

    chore(deps): update deps (#442)

--- test/reporter.test.js
+++ test/reporter.test.js
@@ -3,9 +3,9 @@
 import fs from 'fs';
 import path from 'path';
 
-import middleware from '..';
-
 import statOptions from './fixtures/stat-options';
+
+import middleware from '..';
 
 const statsPath = path.join(__dirname, './fixtures', 'stats.txt');
 const rawStats = fs.readFileSync(statsPath, 'utf8');
