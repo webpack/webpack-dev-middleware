c730a95af1d56481c43fad37e4bcc363cc154a05
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2020-02-04 13:02:22 +0300

    refactor: remove the `watchOptions` option (#565)
    BREAKING CHANGE the `watchOptions` was removed, please setup `watchOptions` in webpack config https://webpack.js.org/configuration/watch/#watchoptions

---
+++ test/fixtures/server-test/webpack.watch-options.config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  context: __dirname,
+  entry: './simple.js',
+  output: {
+    path: path.resolve(__dirname, '../../outputs/'),
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: true,
+  },
+  infrastructureLogging: {
+    level: 'none'
+  },
+};
