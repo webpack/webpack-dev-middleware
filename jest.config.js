c6dd529f5532c1aec37c2e4b9b08c4a6ccfd7680
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2020-05-19 16:00:28 +0300

    chore: deps and fix ci (#641)

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   collectCoverage: false,
-  coveragePathIgnorePatterns: ['test'],
+  coveragePathIgnorePatterns: ['test', '<rootDir>/node_modules'],
   moduleFileExtensions: ['js', 'json'],
   testMatch: ['**/test/**/*.test.js'],
   setupFilesAfterEnv: ['<rootDir>/setupTest.js'],
