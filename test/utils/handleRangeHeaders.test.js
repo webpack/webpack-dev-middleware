bc364053631707e7368f44c2b6adb019255e82db
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-07-13 10:53:24 -0500

    fix(middleware): log error on bad range header

--- test/utils/handleRangeHeaders.test.js
+++ test/utils/handleRangeHeaders.test.js
@@ -1,6 +1,16 @@
 import handleRangeHeaders from '../../src/utils/handleRangeHeaders';
 
 describe('handleRangeHeaders', () => {
+  let context;
+
+  beforeEach(() => {
+    context = {
+      logger: {
+        error: jest.fn(),
+      },
+    };
+  });
+
   it('should return content in range with valid range header', () => {
     const content = 'abcdef';
     const req = {
@@ -13,7 +23,7 @@ describe('handleRangeHeaders', () => {
       setHeader: jest.fn(),
     };
 
-    const contentRes = handleRangeHeaders(content, req, res);
+    const contentRes = handleRangeHeaders(context, content, req, res);
     expect(contentRes).toEqual('bcde');
     expect(res.statusCode).toEqual(206);
     expect(res.setHeader.mock.calls).toMatchSnapshot();
@@ -31,8 +41,9 @@ describe('handleRangeHeaders', () => {
       setHeader: jest.fn(),
     };
 
-    const contentRes = handleRangeHeaders(content, req, res);
+    const contentRes = handleRangeHeaders(context, content, req, res);
     expect(contentRes).toEqual('abcdef');
+    expect(context.logger.error.mock.calls).toMatchSnapshot();
     expect(res.statusCode).toBeUndefined();
     expect(res.setHeader.mock.calls).toMatchSnapshot();
   });
@@ -49,7 +60,7 @@ describe('handleRangeHeaders', () => {
       setHeader: jest.fn(),
     };
 
-    const contentRes = handleRangeHeaders(content, req, res);
+    const contentRes = handleRangeHeaders(context, content, req, res);
     expect(contentRes).toEqual('abcdef');
     expect(res.statusCode).toEqual(416);
     expect(res.setHeader.mock.calls).toMatchSnapshot();
@@ -67,8 +78,9 @@ describe('handleRangeHeaders', () => {
       setHeader: jest.fn(),
     };
 
-    const contentRes = handleRangeHeaders(content, req, res);
+    const contentRes = handleRangeHeaders(context, content, req, res);
     expect(contentRes).toEqual('abcdef');
+    expect(context.logger.error.mock.calls).toMatchSnapshot();
     expect(res.statusCode).toBeUndefined();
     expect(res.setHeader.mock.calls).toMatchSnapshot();
   });
