ea594e46aa7f2a06df46367eee320005e2efad16
evilebottnawi <sheo13666q@gmail.com>
2020-10-15 00:05:36 +0300

    test: refactor

--- test/helpers/GetLogsPlugin.js
+++ test/helpers/GetLogsPlugin.js
@@ -3,19 +3,32 @@ export default class GetLogsPlugin {
     this.logs = [];
   }
 
-  static normalizeLogs(log) {
+  static normalizeLogs(log, type) {
     if (Array.isArray(log)) {
-      return log.map((nestedLog) => GetLogsPlugin.normalizeLogs(nestedLog));
+      return log.map((nestedLog) =>
+        GetLogsPlugin.normalizeLogs(nestedLog, type)
+      );
+    }
+
+    // TODO remove after webpack@4 dropping
+    if (type === 'error') {
+      return 'ERROR';
+    }
+
+    // TODO remove after webpack@4 dropping
+    if (type === 'warn') {
+      return 'WARNING';
+    }
+
+    if (log.includes('modules')) {
+      return 'compiled successfully';
     }
 
     return log
       .toString()
       .trim()
       .replace(process.cwd(), '/absolute/path/to')
-      .replace(/\\/g, '/')
-      .replace(/\d+ modules/, 'X modules')
-      .replace(/Entrypoint (\w+) = ([\w.]+)( (\(.*\)))?/, 'Entrypoint $1 = $2')
-      .replace(/Child "(\w+)":\s+/, 'Child "$1": ');
+      .replace(/\\/g, '/');
   }
 
   apply(compiler) {
@@ -23,7 +36,7 @@ export default class GetLogsPlugin {
       compiler.hooks.infrastructurelog || compiler.hooks.infrastructureLog;
 
     hook.tap('GetLogsPlugin', (name, type, args) => {
-      this.logs.push([name, type, GetLogsPlugin.normalizeLogs(args)]);
+      this.logs.push([name, type, GetLogsPlugin.normalizeLogs(args, type)]);
 
       return false;
     });
