485c12373dd653d006882aa35f3f3e3db8fb4f90
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2023-02-09 11:50:57 +0100

    fix: lint error

--- src/middleware.js
+++ src/middleware.js
@@ -140,9 +140,12 @@ function wrapper(context) {
       }
 
       if (!getHeaderFromResponse(res, "Content-Type")) {
-        // content-type name(like application/javascript; charset=utf-8) or false
         const extname = path.extname(filename);
-        const contentType = extname === ".ts" ? mime.contentType(".js") : mime.contentType(extname); // Only set content-type header if media type is known
+        // content-type name(like application/javascript; charset=utf-8) or false
+        const contentType =
+          extname === ".ts"
+            ? mime.contentType(".js")
+            : mime.contentType(extname);
         // Only set content-type header if media type is known
         // https://tools.ietf.org/html/rfc7231#section-3.1.1.5
         if (contentType) {
