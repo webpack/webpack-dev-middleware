f69b638e0b92278e322524f0104a01905d68bb24
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2024-03-27 19:46:48 +0300

    refactor: code

--- types/middleware.d.ts
+++ types/middleware.d.ts
@@ -3,6 +3,13 @@ export = wrapper;
 /**
  * @template {IncomingMessage} Request
  * @template {ServerResponse} Response
+ * @typedef {Object} SendErrorOptions send error options
+ * @property {Record<string, number | string | string[] | undefined>=} headers headers
+ * @property {import("./index").ModifyResponseData<Request, Response>=} modifyResponseData modify response data callback
+ */
+/**
+ * @template {IncomingMessage} Request
+ * @template {ServerResponse} Response
  * @param {import("./index.js").FilledContext<Request, Response>} context
  * @return {import("./index.js").Middleware<Request, Response>}
  */
@@ -13,9 +20,35 @@ declare function wrapper<
   context: import("./index.js").FilledContext<Request, Response>,
 ): import("./index.js").Middleware<Request, Response>;
 declare namespace wrapper {
-  export { NextFunction, IncomingMessage, ServerResponse, NormalizedHeaders };
+  export {
+    SendErrorOptions,
+    NextFunction,
+    IncomingMessage,
+    ServerResponse,
+    NormalizedHeaders,
+    ReadStream,
+  };
 }
+/**
+ * send error options
+ */
+type SendErrorOptions<
+  Request extends import("http").IncomingMessage,
+  Response extends import("./index.js").ServerResponse,
+> = {
+  /**
+   * headers
+   */
+  headers?: Record<string, number | string | string[] | undefined> | undefined;
+  /**
+   * modify response data callback
+   */
+  modifyResponseData?:
+    | import("./index").ModifyResponseData<Request, Response>
+    | undefined;
+};
 type NextFunction = import("./index.js").NextFunction;
 type IncomingMessage = import("./index.js").IncomingMessage;
 type ServerResponse = import("./index.js").ServerResponse;
 type NormalizedHeaders = import("./index.js").NormalizedHeaders;
+type ReadStream = import("fs").ReadStream;
--- types/utils/compatibleAPI.d.ts
+++ types/utils/compatibleAPI.d.ts
@@ -1,44 +1,13 @@
 /// <reference types="node" />
 export type IncomingMessage = import("../index.js").IncomingMessage;
 export type ServerResponse = import("../index.js").ServerResponse;
-export type ReadStream = import("fs").ReadStream;
-export type ExpectedRequest = {
-  get: (name: string) => string | undefined;
-};
 export type ExpectedResponse = {
   status?: ((status: number) => void) | undefined;
   send?: ((data: any) => void) | undefined;
   pipeInto?: ((data: any) => void) | undefined;
 };
-/**
- * send error options
- */
-export type SendOptions<
-  Request extends import("http").IncomingMessage,
-  Response extends import("../index.js").ServerResponse,
-> = {
-  /**
-   * headers
-   */
-  headers?: Record<string, number | string | string[] | undefined> | undefined;
-  /**
-   * modify response data callback
-   */
-  modifyResponseData?:
-    | import("../index").ModifyResponseData<Request, Response>
-    | undefined;
-  /**
-   * modify response data callback
-   */
-  outputFileSystem: import("../index").OutputFileSystem;
-};
 /** @typedef {import("../index.js").IncomingMessage} IncomingMessage */
 /** @typedef {import("../index.js").ServerResponse} ServerResponse */
-/** @typedef {import("fs").ReadStream} ReadStream */
-/**
- * @typedef {Object} ExpectedRequest
- * @property {(name: string) => string | undefined} get
- */
 /**
  * @typedef {Object} ExpectedResponse
  * @property {(status: number) => void} [status]
@@ -46,59 +15,33 @@ export type SendOptions<
  * @property {(data: any) => void} [pipeInto]
  */
 /**
- * @template {ServerResponse} Response
+ * @template {ServerResponse & ExpectedResponse} Response
  * @param {Response} res
  * @param {number} code
  */
 export function setStatusCode<
-  Response extends import("../index.js").ServerResponse,
+  Response extends import("http").ServerResponse<
+    import("http").IncomingMessage
+  > &
+    import("../index.js").ExtendedServerResponse &
+    ExpectedResponse,
 >(res: Response, code: number): void;
 /**
  * @template {IncomingMessage} Request
  * @template {ServerResponse} Response
- * @typedef {Object} SendOptions send error options
- * @property {Record<string, number | string | string[] | undefined>=} headers headers
- * @property {import("../index").ModifyResponseData<Request, Response>=} modifyResponseData modify response data callback
- * @property {import("../index").OutputFileSystem} outputFileSystem modify response data callback
- */
-/**
- * @template {IncomingMessage} Request
- * @template {ServerResponse} Response
- * @param {Request} req
- * @param {Response} res
- * @param {string} filename
- * @param {number} start
- * @param {number} end
- * @param {() => Promise<void>} goNext
- * @param {SendOptions<Request, Response>} options
+ * @param {Response & ExpectedResponse} res
+ * @param {string | Buffer} bufferOrStream
  */
 export function send<
   Request extends import("http").IncomingMessage,
   Response extends import("../index.js").ServerResponse,
->(
-  req: Request,
-  res: Response,
-  filename: string,
-  start: number,
-  end: number,
-  goNext: () => Promise<void>,
-  options: SendOptions<Request, Response>,
-): Promise<void>;
+>(res: Response & ExpectedResponse, bufferOrStream: string | Buffer): void;
 /**
- * @template {IncomingMessage} Request
  * @template {ServerResponse} Response
- * @param {Request} req response
- * @param {Response} res response
- * @param {number} status status
- * @param {Partial<SendOptions<Request, Response>>=} options options
- * @returns {void}
+ * @param {Response & ExpectedResponse} res
+ * @param {import("fs").ReadStream} bufferOrStream
  */
-export function sendError<
-  Request extends import("http").IncomingMessage,
-  Response extends import("../index.js").ServerResponse,
->(
-  req: Request,
-  res: Response,
-  status: number,
-  options?: Partial<SendOptions<Request, Response>> | undefined,
+export function pipe<Response extends import("../index.js").ServerResponse>(
+  res: Response & ExpectedResponse,
+  bufferOrStream: import("fs").ReadStream,
 ): void;
